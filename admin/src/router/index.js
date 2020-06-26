b6c5b52e04fdbc05a851c087d335eafdb7067f4f
lengxu <17642084810@163.com>
2020-06-26 12:15:01 +0800

    1、增加 welcome 子组件，增加 /admin/welcome 子路由 2、子路由不配置 “/”

--- admin/src/router/index.js
+++ admin/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../views/login.vue'
 import Admin from '../views/admin.vue'
+import Welcome from '../views/admin/welcome'
 
 Vue.use(VueRouter)
 
@@ -19,7 +20,14 @@ const routes = [
     {
         path: '/admin',
         name: 'Admin',
-        component: Admin
+        component: Admin,
+        children: [
+            {
+                path: 'welcome',
+                name: 'Welcome',
+                component: Welcome
+            }
+        ]
     }
 ]
 
